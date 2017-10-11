5b0ca8719e0ec7489acf6aff72b2ede294684472
Odilon Ferreira <woreoaf@gmail.com>
2017-10-10 22:04:22 -0300

    pack constantes

---
+++ Funcional/src/main/java/iu/constantes/UrlPaginas.java
@@ -0,0 +1,13 @@
+package iu.constantes;
+
+public class UrlPaginas {
+
+	static final String DOMINIO = "http://localhost:3000";
+
+	public static final String LOGIN = DOMINIO;
+	public static final String HOME = DOMINIO + "/home";
+
+	private UrlPaginas() {
+	}
+
+}
